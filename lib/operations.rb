8113344e12984b831a64d60d0d3a64e004e7902f
shinik.d.cupo <shinik.d.cupo@gmail.com>
2018-11-15 21:25:45 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,9 @@
-sdef unsafe?(speed)
-   if(speed<30||speed>60)
-    true
-  else false
-  end
-end	end
- def not_safe?(speed)	def not_safe?(speed)
-  speed<30||speed>60 ? true:false
-
-end	end
+def unsafe?(speed)
+    if speed > 60
+        return true
+    elsif speed < 40
+        return true
+    else
+        false
+    end
+ end	
